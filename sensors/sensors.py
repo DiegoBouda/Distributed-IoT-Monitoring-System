1032c743d35fc14b0247ed46ad8e1630f18879ba
Diego Bouda <diegobouda@Diegos-MacBook-Air-4.local>
2025-10-13 20:25:06 -0400

    update sensors.py and add randomDHT.py

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -2,10 +2,12 @@ from time import sleep
 from unittest.mock import Mock
 from PIL import Image
 from threading import Thread
+from randomDHT import randomDHT
+import paho.mqtt.client as paho
 
 class Sensor():
 
-  def __init__(self, camera = None , dht = None, pic_rate=1, dht_rate=1 ):
+  def __init__(self, sensor_id="sensor1", camera = None , dht = None, pic_rate=1, dht_rate=1 ):
     if camera is None:
       self.init_camera()
     else:
@@ -15,8 +17,24 @@ class Sensor():
     else:
       self.dht = dht
     
+    self.client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv311)
+    self.client.on_connect = self.on_connect
+    self.client.on_publish = self.on_publish
+
+    host="localhost"
+    port=1883
+
+    self.client.connect(host,port)
+    self.client.loop_start()
+    
     self.pic_sleep= 1.0 / pic_rate
     self.dht_sleep = 1.0 / dht_rate
+
+    base = f"forest/{sensor_id}"
+    self.temp_topic = f"{base}/temperature"
+    self.humidity_topic = f"{base}/humidity"
+    self.light_topic = f"{base}/light"
+    self.image_topic = f"{base}/image"
 
   def init_dht(self):
     #Ellie - Todo
@@ -35,10 +53,16 @@ class Sensor():
     '''Process Image Data'''
     image = self.camera.capture_image()
     image.show()
+    self.client.publish(self.image_topic, image, 1)
 
   def read_dht(self):
     '''Process Temperature and Humidity Data'''
-    print(f'temp: {self.dht.read_temperature()} humid: {self.dht.read_humidity()}')
+    temp = self.dht.read_temperature()
+    humidity = self.dht.read_humidity()
+    print(f'temp: {temp} humid: {humidity}')
+    self.client.publish(self.temp_topic, temp, 1)
+    self.client.publish(self.humidity_topic, humidity, 1)
+
 
   def photo_loop(self):
     while True:
@@ -66,7 +90,7 @@ if __name__ == "__main__":
   mock_camera = Mock()
   mock_camera.capture_image.return_value = Image.open("xena.jpg")
   
-  dht = Mock()
+  dht = randomDHT()
   dht.read_temperature.return_value = 27.1
   dht.read_humidity.return_value=54.1
 
